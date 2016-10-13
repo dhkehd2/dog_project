7530129961de28648ae90a8db32426341a7c3bfb
USER-PC\USER <gogoy2643@naver.com>
2016-10-13 10:24:41 +0900

    멤버 테이블 생성 및 몇가지 데이터 추가

--- sql/Member_tb.sql
+++ sql/Member_tb.sql
@@ -3,13 +3,18 @@ create table member_tb(
 	mem_pw varchar2(20),
 	mem_name varchar2(20),
 	mem_tel varchar2(20),
-	mem_email varchar2(20),
-	mem_add varchar2(20),
+	mem_email varchar2(30),
+	mem_add varchar2(50),
 	mem_bir date,
 	mem_gen varchar2(20),
 	mem_exp varchar2(20),
-	mem_q varchar2(20),
-	mem_a varchar2(20),
+	mem_q varchar2(100),
+	mem_a varchar2(100),
 	constraint check_exp check( mem_exp IN('yes','no') ),
 	constraint check_gen check( mem_gen IN('male','female') )
 )
+
+drop table member_tb;
+
+insert into member_tb 
+values('gogoy2643','seo','SeoYoonho','010-6330-2643','gogoy2643@naver.com')
