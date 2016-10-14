5d15ec39648ca64297a392a585f089a3ba1e5ec8
yeyoung <jgk8805@naver.com>
2016-10-14 16:46:53 +0900

    세션 아이디 추가

--- src/main/java/third/project/mem_controller/LoginProcessController.java
+++ src/main/java/third/project/mem_controller/LoginProcessController.java
@@ -1,5 +1,7 @@
 package third.project.mem_controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,13 @@ public class LoginProcessController {
 	LoginProcessSvc svc;
 	
 	@RequestMapping(value="loginProcess")
-	public String loginProcess(MemberBean mb) {
+	public String loginProcess(MemberBean mb,HttpSession session) {
 		System.out.println("LoginProcessController.loginProcess()");
 		System.out.println(mb);
 		boolean isCorrectPw = svc.execute(mb);
 		if(isCorrectPw){
 			//로그인이 성공
+			session.setAttribute("mem_id", mb.getMem_id());
 			return "redirect:/list";
 		}else {
 			//로그인이 실패
