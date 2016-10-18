0e5b3b4b404c96d869a523b2759446f3c436bd21
SeoYoonHo <gogoy2643@naver.com>
2016-10-18 17:05:57 +0900

    email commit

--- src/main/java/third/project/mem/svc/Pw_FindProcessSvc.java
+++ src/main/java/third/project/mem/svc/Pw_FindProcessSvc.java
@@ -1,8 +1,9 @@
 package third.project.mem.svc;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import third.project.bean.MemberBean;
@@ -14,23 +15,34 @@ public class Pw_FindProcessSvc {
 	@Autowired
 	MemberDAO dao;
 	
+	@Autowired
+	private MailSender mailSender;
+
 	public MemberBean execute(MemberBean mb) {
 		mb.setMem_bir(mb.getMem_bir() + " 00:00:00");
-		MemberBean mbByDB = dao.pw_findMember(mb);	
+		MemberBean mbByDB = dao.pw_findMember(mb);
 		return mbByDB;
 	}
-	
-	public boolean isEqualAndPassEmail(String a,String b){
+
+	public boolean isEqualAndPassEmail(String a, String b) {
 		boolean res;
-		
-		if(a.equals(b)){
-			//답변 일치하므로 비밀번호 이메일로 보내는 코드 작성
+
+		if (a.equals(b)) {
+			// 답변 일치하므로 비밀번호 이메일로 보내는 코드 작성
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setSubject("hahaha");
+			message.setFrom("yyk1119@gmail.com");
+			message.setText("hihi");
+			message.setTo("gogoy2643@gmail.com");
+			
+			System.out.println(mailSender);
+			mailSender.send(message);
 			res = true;
-		}else{
-			//답변 불일치하므로 비밀번호를 이메일로 보내지 않는다.
+		} else {
+			// 답변 불일치하므로 비밀번호를 이메일로 보내지 않는다.
 			res = false;
 		}
-		
+
 		return res;
 	}
 }
